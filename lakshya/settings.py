204c2df3db16d82284e4afb7318a193f4563c470
Abelboby <abeldavisboby5@gmail.com>
2024-04-02 23:40:45 +0530

    FISRT

--- lakshya/settings.py
+++ lakshya/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hk-ez4*0s9$ufht5el_8(d)d(&xlm8swqudx&*duza&z*5tv@('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.79','127.0.0.1','192.168.37.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
